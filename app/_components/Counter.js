76bd271cfdfd04596d5eb02caa1e22d1bfeca81a
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-07-22 22:28:56 +0200

    Next JS

---
+++ app/_components/Counter.js
@@ -0,0 +1,19 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Counter({ users }) {
+  const [count, setCount] = useState(0);
+
+  console.log(users);
+
+  return (
+    <div>
+      <p>There are {users.length} users</p>
+
+      <button onClick={() => setCount((c) => c - 1)}>-</button>
+      <p>{count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+    </div>
+  );
+}
